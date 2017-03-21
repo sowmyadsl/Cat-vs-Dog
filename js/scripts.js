d73ba90979b53d44b8d8bfa1d67ae1349b8323ca
James Higgins and Sowmya Dinavahi <james@>
2017-03-21 13:54:32 -0700

    Add files to the meow-dog page and add prepend jquery function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("button#meow").click(function() {
+    $("ul#cat-says").prepend("<li>meow!</li>");
+    $("ul#dog-says").prepend("<li>you are so annoying</li>")
+  });
+
+  $("button#bark").click(function() {
+    $("ul#dog-says").prepend("<li>bark</li>");
+    $("ul#cat-says").prepend("<li>you too!</li>");
+  });
+
+});
